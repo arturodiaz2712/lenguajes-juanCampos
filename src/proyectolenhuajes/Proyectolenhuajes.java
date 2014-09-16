26c7d0fce65ae92a872d4d9163b95008d3125b0d
arturodiaz2712 <arturo.diaz.fuentes@gmail.com>
2014-09-15 19:41:49 -0500

    Update Proyectolenhuajes.java

--- src/proyectolenhuajes/Proyectolenhuajes.java
+++ src/proyectolenhuajes/Proyectolenhuajes.java
@@ -16,7 +16,7 @@ public class Proyectolenhuajes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("primer programa");
+        System.out.println("primer programa segundo cambio");
     
     
     
